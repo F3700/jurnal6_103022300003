010b52e33eac0510ac419c4f2c94f6e85e6ccf82
F3700 <albertfebrian26@gmail.com>
2025-03-26 08:12:28 +0700

    penambahan design by contract pada semua kelas

--- Program.cs
+++ Program.cs
@@ -8,6 +8,15 @@ class SayaTubeUser
 
     public SayaTubeUser(String username)
     {
+        if (username.Length > 100)
+        {
+            throw new ArgumentException("Eror -- Username harus kurang dari 100 karakter");
+        }
+        if (username == null)
+        {
+            throw new ArgumentException("Eror -- Username tidak boleh null");
+        }
+
         this.Username = username;
         Random rand = new Random();
         this.id = rand.Next(10000, 99999);
@@ -19,18 +28,31 @@ class SayaTubeUser
         int sum = 0;
         foreach (var item in uploadedVideos)
         {
-            sum += item.getPlayCount();
+            checked
+            {
+                sum += item.getPlayCount();
+            }
         }
         return sum;
     }
-    public void AddVideo(SayaTubeVideo video) {
+    public void AddVideo(SayaTubeVideo video)
+    {
+        if (video == null)
+        {
+            throw new ArgumentException("Eror -- Video tidak boleh null");
+        }
+
+        if (video.getPlayCount() > int.MaxValue)
+        {
+            throw new ArgumentException("Eror -- Playcount kebanyakan");
+        }
         uploadedVideos.Add(video);
     }
     public void PrintAllVideoPlaycount() 
     {
         Console.WriteLine("User: " + this.Username);
 
-        for (int i = 1; i <= uploadedVideos.Count; i++)
+        for (int i = 1; i <= uploadedVideos.Count && i < 9; i++)
         {
             Console.WriteLine("Video "+ i +" judul: " + uploadedVideos[i-1].getJudul());
         }
@@ -46,6 +68,16 @@ class SayaTubeVideo
 
     public SayaTubeVideo(String title)
     {
+        if (title.Length > 200)
+        {
+            throw new ArgumentException("Eror -- Title harus kurang dari 200 karakter");
+        }
+
+        if (title == null)
+        {
+            throw new ArgumentException("Eror -- Title tidak boleh null");
+        }
+
         this.title = title;
         Random rand = new Random();
         this.id = rand.Next(10000, 99999);
@@ -54,7 +86,19 @@ class SayaTubeVideo
 
     public void IncreasePlayCount(int increaseNumb)
     {
-        this.playCount += increaseNumb;
+        if (increaseNumb > 25000000)
+        {
+            throw new ArgumentException("Eror -- increaseNumb tidak boleh lebih dari 25.000.000");
+        }
+
+        if (increaseNumb < 0)
+        {
+            throw new ArgumentException("Eror -- increaseNumb tidak boleh negatif");
+        }
+        checked
+        {
+            this.playCount += increaseNumb;
+        }
     }
     public void PrintVideoDetails()
     {
@@ -138,5 +182,77 @@ class Program
 
         //Test printallvideoplaycpunt
         user1.PrintAllVideoPlaycount();
+
+        Console.WriteLine("\n \n \n");
+        //Judul video memiliki panjang maksimal 200 karakter.
+        try
+        {
+            SayaTubeVideo vid11 = new SayaTubeVideo(new string('A', 202));
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error: " + e.Message);
+        }
+
+        //Input penambahan play count maksimal 25.000.000 per panggilan method-nya
+        try
+        {
+            vid1.IncreasePlayCount(25000001);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error: " + e.Message);
+        }
+
+        //Nama username memiliki panjang maksimal 100 karakter.
+
+        try
+        {
+            SayaTubeUser user2 = new SayaTubeUser(new string('A', 101));
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error: " + e.Message);
+        }
+
+        //Video yang ditambahkan tidak berupa null.
+        try
+        {
+            user1.AddVideo(null);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error: " + e.Message);
+        }
+
+        //testing overflow
+        try
+        {
+            SayaTubeUser user2 = new SayaTubeUser("Febrian");
+            SayaTubeVideo vid20 = new SayaTubeVideo("Review Film \"Dugong Berenang\" oleh Albert Febrian");
+            vid20.IncreasePlayCount(20000000);
+            for (int i = 0; i < 1000000; i++)
+            {
+                user2.AddVideo(vid20);
+            }
+            user2.GetTotalVideoPlayCount();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error: " + e.Message);
+        }
+
+
+        //Jumlah video maksimal yang di-print adalah 8 pada method PrintAllVideoPlaycount()
+        try
+        {
+            Console.WriteLine("\n \n \n");
+            user1.PrintAllVideoPlaycount();
+            //disini hanya terprint 8 walaupun didalamnya ada 10
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error: " + e.Message);
+        }
     }
 }
